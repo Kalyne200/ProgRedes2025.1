6167d424af1c251718470d78e65cd3a1ba03a915
Kalyne200 <kalyne.r@escolar.ifrn.edu.br>
2025-05-16 12:44:07 -0300

    Update q3.py
    CORRIGINDO INFORMAÇÕES

--- q3.py
+++ q3.py
@@ -1,19 +1,22 @@
 import struct
 
+# --- FUNÇÃO DE LEITURA DE ABERTURA E LEITURA DE ARQUIVOS ---
 def ler():
     try:
-        diretorio = "pes.jpg"
+        diretorio = "sapatos.jpg" # INFORME AQUI O NOME DA SUA IMAGEM
         # diretorio = " INFORME AQUI DIRETORIO DA SUA IMAGEM"
         with open (diretorio, "rb") as app1DataSize:
             objeto = app1DataSize.read()
         # leitura do arquivo 1: "app1DataSize"
         
         tamanho = None # tamanho dos metadados
+        
         if len(objeto) >= 6:
-            header = objeto[:6]
-            tamanho = struct.unpack(">H", header[4:6])[0]
+            header_1= objeto[:6]
+            tamanho = struct.unpack(">H", header_1[4:6])[0]
             print(f"tamanho do arquivo app1DataSize é: {tamanho}")
-    except TypeError as e:
+            
+    except TypeError as e: # realiza a verificação de possiveis erros na leitura dos bytes
             print("Erro ao ler tamanho dos metadados: Arquivo muito curto {e}.")
             # obs consudera-se 6 bytes, mas le apenas os bytes 4 - 5
 
@@ -24,15 +27,30 @@ def ler():
                 header_2 = objeto[:4]
                 tamanho = struct.unpack(">H", header_2[2:4])[0]
                 print(f" INGNORE!!")
-    except TypeError as e:
-        print("Erro ao ler tamanho dos metadados: Arquivo muito curto. {e}")
+            # ingora os 4 bytes nas posições 2 e 4
 
+            if len(objeto) >= 18:
+                header = objeto[:18]
+                tamanho = struct.unpack(">H", header[16:18])[0]
+                print(f"tamanho do arquivo app1DataSize é: {tamanho}")
+                
+    except TypeError as e:  # realiza a verificação de possiveis erros na leitura dos bytes
+        print("Erro ao ler tamanho dos metadados: Arquivo muito curto {e}.")
+    # obs consIdera-se os bytes apartir da posição 18
+
+    # --- LER METADADOS ESPECÍFICOS ---
+
+    # Posições dos metadados (convertidas para decimal) AQUI SERA USADA APENAS AS INFORMACÇ~EOS EM HEXA : 
+    # 0X100 PARA DESCUBRIR A POSICAO, LARGURA, ORIENTAÇAÕ E MODELO E EM SEGUIDA O VALOR DOS METADADOS
+    # INDENTIFICADORES DE POSIÇÕES:
+    
     largura = 0x0100 # (Largura)
     altura = 0x0101 # (Altura)
     orientacao = 0x0112 # (Orientação)
     modelo_foto = 0x010E # (Modelo)
     tamanho = 20
 
+    # --- FUNÇÃO DE EXTRAÇÃO DE METADADOS ---
     def extrair(data, posicao, tipo):
         try: # VERIFICANDO POSSIVEIS ERROS...
             tamanho_tipo = struct.calcsize(tipo)
@@ -64,16 +82,6 @@ def ler():
     except Exception as e:
         print(f"Erro ao decodificar o modelo: {e}")
 
-
-    try:
-        if modelo_foto + tamanho <= len(objeto):
-            modelo_bytes = objeto[modelo : modelo_foto + tamanho]
-            modelo = modelo_bytes.decode('utf-8', errors='ignore').rstrip('\x00')
-        else:
-            print(f"Aviso: Posição do modelo ({modelo_foto}) fora dos limites.")
-    except Exception as e:
-        print(f"Erro ao decodificar o modelo: {e}")
-
         print("\n***** METADADOS ENCONTRADOS *****")
         print(f"Largura: {largura}")
         print(f"Altura: {altura}")
@@ -86,9 +94,47 @@ def ler():
     except Exception as e:
         print(f"Ocorreu um erro inesperado: {e}")
 
-
+# --- ATIVANDO A FUNÇÃO PRINCIPAL: "ler" ---
 if __name__ == "__main__":
     ler()
+
+# IMFORMÇÕES:
+
+# OBSERVAÇÃO 1:
+# IMAGEM "SAPATOS" UTILIZADA NA RESOLUÇÃO, E A MESMA IMAGEM DISPONIBILIZADA PELO
+# PROFESSOR NO GOOGLE SALA DE AULA, O ARQUIVO TAMBÉM FOI TESTADO COM CAMINHO DO 
+# DIRETORIO 
+
+# OBSERVAÇÃO 2:
+# Faça um programa que leia os primeiros 6 bytes da imagem JPEG em anexo. Nas
+#posições 4 e 5 há um valor que especifica o tamanho dos metadados presentes nessa
+#imagem. Obtenha esse número (chame-o app1DataSize). Feche o arquivo.
+#a) Abra o arquivo novamente, leia 4 bytes e os ignore. Agora leia o número de
+#bytes em app1DataSize para app1Data. Na posição 16 de app1Data
+#há 2 bytes que indicam quantos metadados essa imagem tem. Descubra-o e
+#informe.
+
+# A PRIMEIRA PARTE DO ARQUIVO REFERENTE A FUNÇÃO: "ler"
+# REALIZA A ABERTURA E LEITURA DO ARQUIVO E SEUS PRIMEIROS 6 BYTES, NAS POSIÇÕES 4 E 5
+# DEPOIS FECHA (COMO USEI WITH OPEN, NÃO FORA USADA CLOSE, APENAS PRINT PARA FORNECER 
+# INFORMAÇÕES
+# DEPOIS ELE ABRI NOVAMENTE O ARQUIVO E LÊ MAIS 4 BYTES, E INGNORA
+# APARTIR DA POSIÇÃO 18 A FUNÇÃO EXTRAI OS METADADOS USANDO AS INFORMAÇÕES DOS 
+# INDENTIFICADORES DE POSIÇÕES (ESSAS INFORMAÇÕES FORAM RETIRADAS DO ARQUIVO:
+# https://exiftool.org/TagNames/EXIF.html
+
+# OS BYTES SÃO EMPACOTADOS EM FORMATO BIG - ENDIAN
+# DEPOIS SÃO DECODIFICADOS, SAINDO DO FORMATO BYTES PARA O FORMATO STRING, 
+# VERIFICANDO POSSIVEIS ERROS NESSE PROCESSO
+
+# EM SEGUIDA A IMPRESSÃO DAS INFORMAÇÕES COM "print"
+
+# VERFICAÇÃO DE ERROS NO ARQUIVO (IMAGEM)
+#  E POR ULTIMO A ATIVAÇÃO "ler" 
+
+
+
+
 
 
 
