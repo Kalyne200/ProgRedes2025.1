ed2dd44d44ea65aff0c36f13d7b55e4528aeed3b
Kalyne200 <kalyne.r@escolar.ifrn.edu.br>
2025-06-13 12:51:22 -0300

    Update q2 - lista2.py
    possui erros

--- q2 - lista2.py
+++ q2 - lista2.py
@@ -1,14 +1,28 @@
-import sys
+import subprocess
 
+def abrir(arquivo):
 
-arquivo = open("sapatos.jpg","rb")
-arquivo.close()
-print(arquivo)
+# abertura e leitura do arquivo 
+    arquivo = open("imagem.jpg","rb")
+    arquivo.read()
+    #arquivo.close()
+    print(arquivo)
 
+# imprime o nome do arquivo
+    if len(sys.argv) < 2:
+        print (f"uso: {sys.argv[0]} nome do arquivo: {arquivo}")
+        sys.exit(1)
 
-if len(sys.argv) < 2:
-    print (f"uso: {sys.argv[0]} nome do arquivo: {arquivo}")
-    sys.exit(1)
+# verifica erros nos comandos 
+    try:
+       subprocess.run(["start", arquivo], shell=True, check=True) 
 
+    except FileNotFoundError:
+            print("Erro: Arquivo não encontrado.")
+        except subprocess.CalledProcessError:
+            print("Erro ao tentar abrir o navegador.")
+    
+    site = https://www.openstreetmap.org/#map=4/-15.13/-53.19
+    abrir(site)
 
 
