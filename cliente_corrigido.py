f810b8d0b142db09bba13009808e84cfabb3185a
Kalyne200 <kalyne.r@escolar.ifrn.edu.br>
2025-08-16 14:41:35 -0300

    Update cliente_corrigido.py

--- cliente_corrigido.py
+++ cliente_corrigido.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import os
 
-ip = '127.0.0.1'
+ip = 'INFORME O IP'
 porta = 2121
 pasta_arquivos = 'arquivos'
 code = 'utf-8'
@@ -84,3 +84,4 @@ thread_usuario.join()
 thread_servidor.join()
 
 cliente_socket.close()
+
